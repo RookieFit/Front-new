eedd04cd0f44f7ae3677369f0c4b6a152e3aa620
동영 <57888678+dahli4@users.noreply.github.com>
2025-01-09 23:56:29 +0900

    [Feat] #10 사이드바 메뉴 토글
    fe

--- src/pages/Layout/index.jsx
+++ src/pages/Layout/index.jsx
@@ -1,16 +1,38 @@
-import { Outlet } from "react-router-dom";
-import SideBar from "../../components/SideBar/SideBar";
+import React, { useState } from 'react';
+import { Outlet } from 'react-router-dom';
+import SideBar from '../../components/SideBar/SideBar';
 
 const Layout = () => {
-    return (
-        <div className="flex h-screen">
-            <nav className="w-64 h-full bg-gray-800 text-white p-4">
-                <SideBar />
-            </nav>
+    const [isCollapsed, setIsCollapsed] = useState(false);
 
-            <main className="flex-1 p-6 bg-gray-100">
-                <Outlet />
-            </main>
+    const toggleSidebar = () => {
+        setIsCollapsed(!isCollapsed);
+    };
+
+    return (
+        <div className="bg-rookieRed">
+            <header className="flex items-center bg-rookieFitRed text-white p-4">
+                <h2 className="text-3xl font-semibold">Rookie Fit</h2>
+                <button
+                    onClick={toggleSidebar}
+                    className="text-white hover:text-gray-300 mr-4"
+                >
+                    <span className="material-icons ml-12 text-2xl font-bold">
+                        {isCollapsed ? '>>' : '<<'}
+                    </span>
+                </button>
+            </header>
+
+            <div className="flex h-screen">
+                <SideBar
+                    isCollapsed={isCollapsed}
+                    toggleSidebar={toggleSidebar}
+                />
+
+                <main className="flex-1 p-6 bg-gray-100">
+                    <Outlet />
+                </main>
+            </div>
         </div>
     );
 };

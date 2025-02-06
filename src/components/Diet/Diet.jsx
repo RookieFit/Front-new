61ba262cb7e38f637db2e3deaebeed0409459700
kyoungeun <91576339+21270003@users.noreply.github.com>
2025-02-06 23:42:09 +0900

    feat: 다이어트 전체 틀 구현
    틀 구현 및 경로 지정

---
+++ src/components/Diet/Diet.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Diet = () => {
+  return (
+    <div className="w-full h-full bg-rookieHover rounded-lg flex items-center justify-center">
+      {/* 내부 박스 */}
+      <div className="relative w-[97%] h-[90vh] bg-white rounded-lg flex items-center justify-center"></div>
+    </div>
+  );
+};
+
+export default Diet;
--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -2,26 +2,31 @@ import { createBrowserRouter } from "react-router-dom";
 import Layout from "../pages/Layout";
 import LoginPage from "../pages/Login/LoginPage";
 import Workout from "../components/Workout/Workout";
+import Diet from "../components/Diet/Diet";
 
 const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <Layout />,
-        children: [
-            {
-                path: "",
-                element: <div>내용이 들어가야 합니다요</div>,
-            },
-            {
-                path: "/workout",
-                element: <Workout />,
-            },
-        ],
-    },
-    {
-        path: "/login",
-        element: <LoginPage />,
-    },
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "",
+        element: <div>내용이 들어가야 합니다요</div>,
+      },
+      {
+        path: "/workout",
+        element: <Workout />,
+      },
+      {
+        path: "/diet",
+        element: <Diet />,
+      },
+    ],
+  },
+  {
+    path: "/login",
+    element: <LoginPage />,
+  },
 ]);
 
 export default router;

9914da3664b643bc7b9b1f5aa8350b75ea3db436
kyoungeun <91576339+21270003@users.noreply.github.com>
2025-02-07 00:00:04 +0900

    chore: 박스 내부에 세로 라인 추가

--- src/components/Diet/Diet.jsx
+++ src/components/Diet/Diet.jsx
@@ -4,7 +4,10 @@ const Diet = () => {
   return (
     <div className="w-full h-full bg-rookieHover rounded-lg flex items-center justify-center">
       {/* 내부 박스 */}
-      <div className="relative w-[97%] h-[90vh] bg-white rounded-lg flex items-center justify-center"></div>
+      <div className="relative w-[97%] h-[90vh] bg-white rounded-lg flex items-center justify-center">
+        {/* 세로로 얇은 선 */}
+        <div className="absolute left-1/2 transform -translate-x-1/2 h-[70%] border-l-2 border-gray-200"></div>
+      </div>
     </div>
   );
 };

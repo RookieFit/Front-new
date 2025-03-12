d816af01bb7838d77e9ebc51f027c0df372ccb15
mjKim <106668821+pipakmj@users.noreply.github.com>
2025-03-12 22:12:04 +0900

    feat: 내 게시글 검색기능 추가(댓글관련 검색은 추후 추가)

--- src/components/Community/MyCommunityBoard.jsx
+++ src/components/Community/MyCommunityBoard.jsx
@@ -1,9 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import BoardComponent from './BoardComponent';
 import ApiClient from '../../services/ApiClient';
+import CommunitySearchSection from './CommunitySearchSection';
 
 const MyCommunityBoard = () => {
 
+    const [searchType, setSearchType] = useState('제목만');
+    const [searchQuery, setSearchQuery] = useState('');
     const [boardList, setBoardList] = useState([]);
     const [selectedType, setSelectedType] = useState('전체');
     const [currentPage, setCurrentPage] = useState(1);
@@ -30,16 +33,54 @@ const MyCommunityBoard = () => {
         fetchBoardList();
     }, [selectedType, currentPage]);
 
+    const handleSearch = (type, query) => {
+
+        setCurrentPage(1);
+
+        const fetchBoardList = async () => {
+            try {
+                let url = `/user/community/mylist?page=0&size=${itemsPerPage}`;
+                if (type !== '전체') {
+                    url += `&communityType=${selectedType}`;
+                }
+
+                if (query.trim()) {
+                    url += `&searchType=${type}&searchQuery=${encodeURIComponent(query)}`;
+                }
+
+                const response = await ApiClient.get(url);
+                setBoardList(response.data.content || []);
+                setTotalPages(response.data.totalPages || 1);
+            } catch (error) {
+                console.error("게시글 불러오기 실패:", error);
+                setBoardList([]);
+            }
+        };
+
+        fetchBoardList();
+    };
+
     return (
         <div>
-            <BoardComponent
-                boardList={boardList}
-                totalPages={totalPages}
-                setSelectedType={setSelectedType}
-                selectedType={selectedType}
-                currentPage={currentPage}
-                setCurrentPage={setCurrentPage}
-            />
+            <section className='h-[40px] mb-3'>
+                <CommunitySearchSection
+                    searchType={searchType}
+                    setSearchType={setSearchType}
+                    searchQuery={searchQuery}
+                    setSearchQuery={setSearchQuery}
+                    handleSearch={handleSearch}
+                />
+            </section>
+            <section>
+                <BoardComponent
+                    boardList={boardList}
+                    totalPages={totalPages}
+                    setSelectedType={setSelectedType}
+                    selectedType={selectedType}
+                    currentPage={currentPage}
+                    setCurrentPage={setCurrentPage}
+                />
+            </section>
         </div>
     );
 };
